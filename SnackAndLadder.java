0dd98baa9b5af5b60ff57f2e5d1d75c1f23ca6fb
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-07-13 13:32:37 +0530

    Start the game with starting position

--- SnackAndLadder.java
+++ SnackAndLadder.java
@@ -2,6 +2,8 @@ public class SnackAndLadder {
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to Snake and Ladder Simulation Program");
+		int player=0;
+		System.out.println("Snake and Ladder game start with a single player ");
+		System.out.println("Starting Position of the Player is  "+player );
 	}
 }
